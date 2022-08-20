67808bf2835d7e9001ed44f696f9e409aab0a4cf
jitmanew-tyagi <jitmanew@gmail.com>
2022-08-20 22:03:02 +0530

    OOPS CLASS

---
+++ Car.java
@@ -0,0 +1,28 @@
+public class Car {
+    public int average;
+    public String colour;
+    public int power;
+    public int torque;
+    public static int cost = 100000;
+    public int[] dim;
+
+    public void run(int speed) {
+        System.out.println("up and running at" + speed);
+    }
+
+    public void showInfo() {
+        
+        System.out.println("Average: " + this.average);
+        System.out.println("Power: " + this.power);
+    }
+
+    public void blowHorn() {
+        System.out.println("beeeep");
+    }
+
+    public static int getCost() {
+        System.out.println(cost);
+        return cost;
+    }
+
+}
---
+++ OOPs.java
@@ -0,0 +1,21 @@
+public class OOPs {
+    public static void main(String[] args) {
+        Car alto = new Car();
+        // Car i10 = new Car();
+        // alto.average = 20;
+        // alto.colour = "black";
+        // alto.power = 1000;
+        // // i10.power = 1200;
+        // i10.average = 120;
+
+        // // System.out.println(alto.average);
+        // // System.out.println(alto.power);
+        // alto.run(30);
+        // alto.showInfo();
+        // i10.showInfo();
+        alto.dim = new int[] {100, 80};
+        System.out.println(alto.dim[0] + " " + alto.dim[1]);
+    //     System.out.println(Car.cost);
+    //     System.out.println(Car.getCost());
+    }
+}
